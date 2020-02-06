d53b948a85aa7c34fe61c19d1e2143592bcae2ab
Prabeena <prabeenapanda97@gmail.com>
2020-02-06 15:48:59 +0530

    added 2 files

---
+++ 2 list to a dict.py.py
@@ -0,0 +1,8 @@
+# Convert two lists into a dictionary.
+test_keys = ["Rash", "Kil", "Varsha"]
+test_values = [1, 4, 5]
+res = {}
+for key in test_keys:
+    for value in test_values:
+        res[key] = value
+print(str(res))
